7269f049f6c8efe7d21c75091cb9bf351f57fe50
John Rucker <John.Rucker@Solar-Current.com>
2019-02-04 15:18:53 -0600

    ed

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//const sunnyBoyWebBox =  require('sunnyboy-web-box-data-fetcher');
-const rvrDta =          require('river-data-fetcher');
-const MyAppMan =        require('./MyAppManager.js');
+const rvrDta =      require('river-data-fetcher');
+const MyAppMan =    require('./MyAppManager.js');
 
 const myAppMan = new MyAppMan();
 
 console.log('__________________ App Config follows __________________');
 console.dir(myAppMan.config, {depth: null});
 console.log('________________________________________________________');
-
-//var solarData =  new sunnyBoyWebBox(myAppMan.config.webBoxIP);
 
 var siteList = {
     [myAppMan.config.dataSiteCode] : {descripition:[myAppMan.config.descripition]}
@@ -21,7 +18,6 @@ function dataUpdateInstantValues(){
         if(errNum == 0){   
             //console.log('dobj follows:');
             //console.dir(dObj,{depth: null});
-
             var rvrLevel = dObj[myAppMan.config.dataLoc][myAppMan.config.dataSiteCode].siteData[myAppMan.config.dataParCode].value
             console.log('River Level = ' + rvrLevel);
 
@@ -38,42 +34,5 @@ function dataUpdateInstantValues(){
     });
 }
 
-
-
 setTimeout(()  =>{dataUpdateInstantValues();}, 5000);                     // wait 5 seconds and then send gauge values (only ran once)
-
-/*
-setInterval(() =>{getRiverLevel();}, 5 * 60 * 1000);            // every 5 minutes 
-
-
-function getRiverLevel(){
-
-    
-    solarData.updateValues(function(errNumber, errTxt, dtaObj){
-        if(errNumber == 0){
-            console.log('Currently generating ' + dtaObj.powerNow + " " + dtaObj.powerNowUnit);
-            console.log("\tToday's total power = " + dtaObj.powerToday + " " + dtaObj.powerTodayUnit);
-            console.log('\tTotal all time power generated = '+ dtaObj.powerTotal + " " + dtaObj.powerTotalUnit);
-            if(myAppMan.setGaugeValue(dtaObj.powerNow)){
-                myAppMan.setGaugeStatus('Okay, ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
-            } else {
-                console.log('Not allowed to send gaguge value at this time by AppManager.  Check IOS App for details');
-            };
-        } else {
-            console.log('Error getting data from Sunnyboy WebBox');
-            console.log(errTxt);
-            myAppMan.setGaugeStatus('Error getting data from SunnyBoy Webbox at ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString()  + ' -> '+ errTxt);
-        };
-    });
-    
-};
-
-myAppMan.on('Update', ()=>{
-    console.log('New update event has fired.  Reloading gauge objects...');
-    myAppMan.setGaugeStatus('Config updated received. Please wait, may take up to 5 minutes to reload gauge objects. ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
-    console.log('The webBoxIP = ' + myAppMan.config.webBoxIP);
-    //solarData =  new sunnyBoyWebBox(myAppMan.config.webBoxIP);
-    getRiverLevel();
-});
-
-*/
+setInterval(() =>{dataUpdateInstantValues();}, 10 * 60 * 1000);           // every 10 minutes 
